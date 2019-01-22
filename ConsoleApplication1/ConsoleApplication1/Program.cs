3d6246b8ea61f35918cdc81af4c101fc9bd5320a
nor\anescus <silviu.anescu@vizolution.co.uk>
2019-01-22 19:14:40 +0200

    Rs

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -9,7 +9,7 @@ namespace ConsoleApplication1
     class Program
     {
         static void Main(string[] args)
-        {
+        {asdasdsad
             Console.WriteLine("Test");
         }
     }
